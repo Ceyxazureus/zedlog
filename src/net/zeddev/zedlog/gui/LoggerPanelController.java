1da3f7c4e6d2077b7a8330b6866d2478919b1966
zscott92 <zscott.dev@gmail.com>
2013-04-28 12:02:46 +1000

    Tidied up log output on the LoggerPanel.

--- src/net/zeddev/zedlog/gui/LoggerPanelController.java
+++ src/net/zeddev/zedlog/gui/LoggerPanelController.java
@@ -1,6 +1,7 @@
 
 package net.zeddev.zedlog.gui;
 
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import net.zeddev.zedlog.logger.DataLogger;
 import net.zeddev.zedlog.logger.DataLoggerObserver;
@@ -40,12 +41,37 @@ public final class LoggerPanelController implements DataLoggerObserver {
 		shutdown();
 	}
 
+	private DataLogger lastToNotify = null;
+
+	private void addLog(final DataLogger logger, final LogEntry logEntry) {
+
+		StringBuilder logEntries = new StringBuilder(loggerPanel.getTxtLogEntries().getText());
+
+		// add newline to separate different logger messages
+		if (lastToNotify == null) {
+			lastToNotify = logger;
+		} else if (logger != lastToNotify) {
+
+			// dont append if already a newline
+			if (logEntries.charAt(logEntries.length()-1) != '\n')
+				logEntries.append("\n");
+
+			lastToNotify = logger;
+
+		}
+
+		logEntries.append(logEntry.getMessage());
+
+		loggerPanel.getTxtLogEntries().setText(logEntries.toString());
+
+	}
+
 	@Override
 	public void notifyLog(final DataLogger logger, final LogEntry logEntry) {
 
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run(){
-				loggerPanel.getTxtLogEntries().append(logEntry.getMessage());
+				addLog(logger, logEntry);
 			}
 		});
 
