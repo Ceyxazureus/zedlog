8b0f570049c61122299e75b980f5def920d090d1
zscott92 <zscott.dev@gmail.com>
2013-05-05 17:16:47 +1000

    Added clear all loggers & logged data option.

--- src/net/zeddev/zedlog/gui/ZedLogFrameController.java
+++ src/net/zeddev/zedlog/gui/ZedLogFrameController.java
@@ -23,7 +23,6 @@ import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
 import javax.swing.JFileChooser;
 import javax.swing.JTabbedPane;
 import net.zeddev.litelogger.Logger;
@@ -101,12 +100,6 @@ public final class ZedLogFrameController implements NativeMouseListener {
             }
         });
 
-		frame.getBtnClear().addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                btnClearActionPerformed(event);
-            }
-        });
-
 		frame.getBtnNext().addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 btnNextActionPerformed(event);
@@ -134,12 +127,6 @@ public final class ZedLogFrameController implements NativeMouseListener {
 		frame.getMItemRemove().addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 mitemRemoveActionPerformed(evt);
-            }
-        });
-
-		frame.getMItemClear().addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mitemClearActionPerformed(evt);
             }
         });
 
@@ -326,31 +313,6 @@ public final class ZedLogFrameController implements NativeMouseListener {
 	private void btnRemoveActionPerformed(final ActionEvent event) {
 		removeDataLogger();
     }
-
-	private void clearAllLoggers() {
-
-		try {
-			loggers.clear();
-		} catch (IOException ex) {
-			logger.error("Failed to clear loggers!", ex);
-		}
-
-		// clear the loggers tabs
-		while (frame.getTabs().getTabCount() > 0)
-			frame.getTabs().remove(0);
-
-		// put the composite logger back on the frame
-		addLoggerTab(loggers);
-
-	}
-
-	private void btnClearActionPerformed(final ActionEvent event) {
-		clearAllLoggers();
-    }
-
-	private void mitemClearActionPerformed(final ActionEvent event) {
-		clearAllLoggers();
-	}
 
 	private void btnNextActionPerformed(final ActionEvent event) {
 
