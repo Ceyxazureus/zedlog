75c00f87f6318a39a779bc5901d20c7ea79e8012
zscott92 <zscott.dev@gmail.com>
2013-08-01 11:47:10 +1000

    Fixed bug in MouseEvent.fromXML().

--- src/net/zeddev/zedlog/logger/impl/event/MouseEvent.java
+++ src/net/zeddev/zedlog/logger/impl/event/MouseEvent.java
@@ -132,7 +132,7 @@ public abstract class MouseEvent extends LogEvent {
 			parent.getAttribute("x")
 		));
 
-		setX(Integer.parseInt(
+		setY(Integer.parseInt(
 			parent.getAttribute("y")
 		));
 		
