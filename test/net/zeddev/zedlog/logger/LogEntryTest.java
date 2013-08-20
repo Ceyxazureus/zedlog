23991e136fbe4a46144f4bea98b3e01e73861d2c
zscott92 <zscott.dev@gmail.com>
2013-08-20 17:43:13 +1000

    Added LogEntry unit test.

--- test/net/zeddev/zedlog/logger/LogEntryTest.java
+++ test/net/zeddev/zedlog/logger/LogEntryTest.java
@@ -1,4 +1,4 @@
-package net.zeddev.zedlog.logger.impl;
+package net.zeddev.zedlog.logger;
 /* Copyright (C) 2013  Zachary Scott <zscott.dev@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
@@ -15,8 +15,15 @@ package net.zeddev.zedlog.logger.impl;
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import net.zeddev.zedlog.logger.impl.DataLoggers;
+import net.zeddev.zedlog.logger.impl.LogEvents;
+import net.zeddev.zedlog.logger.impl.event.KeyEvent;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Unit test for {@link net.zeddev.zedlog.logger.LogEntry}.
@@ -28,13 +35,51 @@ public final class LogEntryTest {
 	/** Tests the {@code LogEntry} creation/instantiation. */
 	@Test
 	public void testLogEntry() throws Throwable {
-		fail("Not yet implemented");
+		
+		DataLogger dl = DataLoggers.newDataLogger("CharTyped");
+		String msg = "Test LogEntry Message.";
+		LogEvent event = LogEvents.newLogEvent("KeyEvent");
+		
+		long startTime = System.currentTimeMillis();
+		LogEntry inst = new LogEntry(dl, msg, event);
+		
+		assertEquals(inst.getParent(), dl);
+		assertEquals(inst.getMessage(), msg);
+		assertEquals(inst.getEvent(), event);
+		assertTrue(inst.getTimestamp() >= startTime); // should be AFTER instantiation
+		
 	}
 	
 	/** Tests the {@code LogEntry}s {@code toXML()} and {@code fromXML()} methods. */
 	@Test
 	public void testLogEntryXML() throws Throwable {
-		fail("Not yet implemented");
+		
+		DataLogger dl = DataLoggers.newDataLogger("CharTyped");
+		String msg = "Test LogEntry Message.";
+		LogEvent event = new KeyEvent(KeyEvent.Type.TYPED, 1, 'a');
+		
+		long startTime = System.currentTimeMillis();
+		LogEntry inst = new LogEntry(dl, msg, event);
+		
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+		Document xmlLog = docBuilder.newDocument();
+
+		// NOTE dont need rest of document, just an <entries>
+		Element entries = xmlLog.createElement("entries");
+
+		inst.toXML(entries);
+		
+		// parse xml
+		LogEntry readEntry = new LogEntry();
+		readEntry.fromXML(
+			(Element) entries.getElementsByTagName("entry").item(0)
+		);
+		
+		// check read correctly
+		assertEquals(readEntry, inst);
+		
 	}
-	
+
 }
